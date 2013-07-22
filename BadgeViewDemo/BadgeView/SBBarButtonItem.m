39b128ea185d953fe59970405027f17081acf239
Pierluigi Cifani <pcifani@voalte.com>
2013-07-22 13:45:58 +0200

    Fix compiler warning

--- BadgeViewDemo/BadgeView/SBBarButtonItem.m
+++ BadgeViewDemo/BadgeView/SBBarButtonItem.m
@@ -36,7 +36,7 @@
     else
     {
         
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, nil), ^{
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             
             UIImage *orginalImage = self.originalImage;
 
